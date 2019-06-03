ab7538475e16813f9cbc926c99a1b71f46cb43ce
YPHNCKU <N26070033@mail.ncku.edu.tw>
2019-06-03 16:01:07 +0800

    Add file input and a graph file

--- main.cc
+++ main.cc
@@ -12,8 +12,13 @@ using namespace std;
 NetworkManager *nm = new NetworkManager();
 
 int main(int argc, char** argv){
-
+	
+	if(argc<2)
+		printf("No input graph!!\n");
+	else
+		nm->intrepret(argv[1]);
     /* start your program */
-
+	nm->print_all_v();
+	nm->print_all_e();
     return 0;
 }
